652d06f94d6be37e7a227784c2a2d09b8ef31f37
Tina Wang <tswang@caltech.edu>
2017-10-15 14:48:53 -0700

    Initial Commit

---
+++ Clear-Vowels/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clear-Vowels
+//
+//  Created by Tina Wang on 10/15/17.
+//  Copyright © 2017 Tina Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
